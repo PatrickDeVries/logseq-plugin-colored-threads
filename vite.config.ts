877e0edae8c11f1c472bc4d8042d198c1b5f0d0f
Patrick DeVries <pdevries@headstorm.com>
2023-11-14 22:27:45 -0600

    Add logo and example image

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
-import logseqDevPlugin from 'vite-plugin-logseq'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [logseqDevPlugin()],
-  // Makes HMR available for development
   build: {
     target: 'esnext',
     minify: 'esbuild',
