f59088ee78105096b4d33132067f3c5c3f0ff95b
Patrick DeVries <pdevries@headstorm.com>
2023-11-14 22:47:08 -0600

    Make bar fill optional and add settings screenshot to README

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+import logseqDevPlugin from 'vite-plugin-logseq'
 
 export default defineConfig({
+  plugins: [logseqDevPlugin()],
   build: {
     target: 'esnext',
     minify: 'esbuild',
